d93859c189777f34bd92044c128fcf81e00f85ba
Karel Vervaeke <karel.vervaeke@conundra.eu>
2023-12-11 22:46:54 +0100

    euler 6

--- kotlin/src/main/kotlin/info/vervaeke/euler/Euler01.kt
+++ kotlin/src/main/kotlin/info/vervaeke/euler/Euler01.kt
@@ -5,7 +5,8 @@ fun main() {
 //    println("Euler 2 ${euler02()}")
 //    println("Euler 3 ${euler03()}")
 //    println("Euler 4 ${euler04()}")
-    println("Euler 5 ${euler05()}")
+//    println("Euler 5 ${euler05()}")
+    println("Euler 5 ${euler06()}")
 }
 
 private fun euler01() = (0 until 1000).filter { it % 3 == 0 || it % 5 == 0 }.sum()
@@ -51,12 +52,20 @@ private fun euler05(): Long {
     return euler05(20)
 }
 
-private fun euler05(d: Long):Long {
+private fun euler05(d: Long): Long {
     var r = 1L
     while (true) {
-        if ((1 until d).all { r%it == 0L }) {
+        if ((1 until d).all { r % it == 0L }) {
             return r
         }
         r++
     }
+}
+
+fun euler06(): Long {
+    val n = 100L
+    val a = (0..n).sumOf { it * it }
+    val b = n * n * (n + 1) * (n + 1) / 4
+
+    return b - a
 }
