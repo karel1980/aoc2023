1b3735e75e2db86c70c3f6bd2904c333ebfbc356
Karel Vervaeke <karel.vervaeke@conundra.eu>
2023-12-11 08:46:41 +0100

    Day 11 cleanup

--- kotlin/src/main/kotlin/info/vervaeke/aoc2023/day11/Day11.kt
+++ kotlin/src/main/kotlin/info/vervaeke/aoc2023/day11/Day11.kt
@@ -10,6 +10,12 @@ data class Coord(val row: Int, val col: Int) {
 data class Day11(val grid: List<String>) {
     val rows = 0 until grid.size
     val cols = 0 until grid[0].length
+    val emptyRows = rows.filter {
+        '#' !in grid[it]
+    }
+    val emptyCols = cols.filter { col ->
+        rows.all { grid[it][col] != '#' }
+    }
 
     companion object {
         fun parseInput(path: String) = parseLines(readInput(path))
@@ -23,23 +29,21 @@ data class Day11(val grid: List<String>) {
         return calculateSpace(2)
     }
 
-    fun calculateSpace(emptySpace: Long): Long {
-        val galaxies = findGalaxies()
-        val emptyRows = emptyRows()
-        val emptyCols = emptyCols()
-
-        var result = 0L
-        galaxies.forEach { a ->
-            galaxies.forEach { b ->
-                val distance = distance(a, b, emptyRows, emptyCols, emptySpace)
-                println("$a to $b: $distance")
-                result += distance
-            }
-        }
-        return result / 2
+    fun part2(): Long {
+        return calculateSpace(1000000L)
     }
 
-    fun distance(a: Coord, b: Coord, emptyRows: List<Int>, emptyCols: List<Int>, emptySpace: Long = 2): Long {
+    fun calculateSpace(emptySpace: Long = 2): Long {
+        val galaxies = findGalaxies()
+
+        return galaxies.sumOf { a ->
+            galaxies.sumOf { b ->
+                distance(a, b, emptySpace)
+            }
+        } / 2
+    }
+
+    fun distance(a: Coord, b: Coord, emptySpace: Long = 2): Long {
         val d = abs(b.col - a.col) + abs(b.row - a.row)
 
         val rplus = (min(a.row, b.row) until max(a.row, b.row)).count { it in emptyRows }
@@ -72,16 +76,11 @@ data class Day11(val grid: List<String>) {
         }
     }
 
-    fun part2(): Long {
-        return calculateSpace(1000000L)
-    }
-
 
 }
 
 fun main() {
     val day = Day11.parseInput("input")
     println("Part 1: ${day.part1()}")
-    //6787208808158 is too high
     println("Part 2: ${day.part2()}")
 }
--- kotlin/src/test/kotlin/info/vervaeke/aoc2023/day11/Day11Test.kt
+++ kotlin/src/test/kotlin/info/vervaeke/aoc2023/day11/Day11Test.kt
@@ -15,14 +15,14 @@ class Day11Test {
 
     @Test
     fun zero() {
-        assertThat(day11.findGalaxies().all { day11.distance(it, it, day11.emptyRows(), day11.emptyCols()) == 0L })
+        assertThat(day11.findGalaxies().all { day11.distance(it, it) == 0L })
             .isTrue()
     }
 
     @Test
     fun distance() {
         val gal = day11.findGalaxies()
-        assertThat(day11.distance(gal[4], gal[8], day11.emptyRows(), day11.emptyCols()))
+        assertThat(day11.distance(gal[4], gal[8]))
             .isEqualTo(9)
     }
 
@@ -30,6 +30,12 @@ class Day11Test {
     fun emptyRows() {
         assertThat(day11.emptyRows())
             .isEqualTo(listOf(3, 7))
+    }
+
+    @Test
+    fun emptyCols() {
+        assertThat(day11.emptyCols())
+            .isEqualTo(listOf(2, 5, 8))
     }
 
     @Test
@@ -41,15 +47,23 @@ class Day11Test {
     @Test
     fun part2() {
         assertThat(day11.part2())
-            .isEqualTo(42)
+            .isEqualTo(82000210L)
     }
+
     @Test
-    fun calculateEmptySpace10() {
+    fun part2Real() {
+        assertThat(Day11.parseInput("input").part2())
+            .isEqualTo(678728808158L)
+    }
+
+    @Test
+    fun calculateSpace10() {
         assertThat(day11.calculateSpace(10))
             .isEqualTo(1030)
     }
+
     @Test
-    fun calculateEmptySpace1000() {
+    fun calculateSpace1000() {
         assertThat(day11.calculateSpace(100))
             .isEqualTo(8410)
     }
