b6bb3f878d2e3ed59eb95d5769183f10a74c0387
Karel Vervaeke <karel.vervaeke@conundra.eu>
2023-12-12 21:15:50 +0100

    day12 part 2
    part 2 forced total rewrite

--- kotlin/src/main/kotlin/info/vervaeke/aoc2023/day12/Day12.kt
+++ kotlin/src/main/kotlin/info/vervaeke/aoc2023/day12/Day12.kt
@@ -4,38 +4,116 @@ import java.util.*
 import java.util.regex.Pattern
 
 data class Row(val spec: String, val brokenGroups: List<Int>) {
+    // indices of question marks in the spec
     val unknowns = spec.indices.filter { spec[it] == '?' }.mapIndexed { idx, pos -> pos to idx }.toMap()
+
+    // number of '?' or '#' starting from the current location
+    val numberOfBrokenCandidatesRightOfLocation = spec.indices.map {
+        spec.substring(it).count { it == '?' || it == '#' }
+    }
+
+    // given number of remaining broken positions, what's the right most position that has that many positions available
+    fun remainingBrokenStartingAt(pos: Int): Int {
+        return spec.length - pos
+    }
 
     val pattern = Pattern.compile("\\.+")
 
-    val options = ".#"
-
-    fun countArrangements(): Int {
-        if (unknowns.size == 0) {
+    fun countArrangements(): Long {
+        if (unknowns.isEmpty()) {
             return 1
         }
 
-        val numberOfCombinations = Math.pow(2.toDouble(), unknowns.size.toDouble()).toInt()
-        return (0 until numberOfCombinations).count { n ->
-            val bitset = createBitSet(n)
-            val instance = spec.indices.map {
-                spec[it].takeIf { it != '?' } ?: options[if (bitset[unknowns[it]!!]) {
-                    0
-                } else {
-                    1
-                }]
-            }.joinToString("") { it.toString() }
-            matchesInstance(instance)
+        val arrangements = brokenGroups.map { spec.map { 0L }.toMutableList() }
+        // arrangements[i][p] holds the number of arrangements that are possible for element i at position p
+        // a 0 means it can't be at that position
+        var i = brokenGroups.size - 1
+        while (i >= 0) {
+            // calculate arrangements for group i
+            val groupSize = brokenGroups[i]
+            (0 until spec.length).forEach { pos ->
+                if (groupCanBeAt(i, pos)) {
+                    if (i == brokenGroups.size - 1) {
+                        arrangements[i][pos] = 1
+                    } else {
+                        if (pos + groupSize + 2 <= spec.length) {
+                            // only count arrangements if there is no # between this group and the next
+                            arrangements[i][pos] = (pos + groupSize + 1 until spec.length).filter {
+                                !spec.substring(pos + groupSize+1, it).contains("#")
+                            }.sumOf {
+                                arrangements[i+1][it]
+                            }
+                        }
+                    }
+                }
+            }
+            i--
+        }
+
+        // return the sum of arrangements for group 0
+        return arrangements[0].sum()
+    }
+
+    fun groupCanBeAt(groupNum: Int, pos: Int): Boolean {
+        val groupSize = brokenGroups[groupNum]
+        if (pos + groupSize > spec.length) {
+            // not enough room
+            return false
+        }
+        if (spec.substring(pos, pos + groupSize).contains(".")) {
+            // spec part contains non broken part
+            return false
+        }
+        if (groupNum == 0) {
+            // extra check: there cannot be a broken item before the first group
+            if (spec.substring(0, pos).contains("#")) {
+                return false
+            }
+        } else {
+            // there must be at least one blank before every group except the first
+            if (pos == 0) {
+                return false
+            }
+            if (spec[pos - 1] != '.' && spec[pos - 1] != '?') {
+                return false
+            }
+        }
+        if (groupNum == brokenGroups.size - 1) {
+            // there should be no broken parts after this one
+            if (pos + groupSize < spec.length && (spec.substring(pos + groupSize).contains("#"))) {
+                return false
+            } else {
+                return true
+            }
+        } else {
+            // there must be a dot after this one
+            if (spec.substring(pos + groupSize).startsWith(".") || spec.substring(pos + groupSize).startsWith("?")) {
+                return true
+            } else {
+                return false
+            }
         }
     }
 
-    fun unfold(): Row {
-        return Row(listOf(spec, spec, spec, spec).joinToString("?") { it }, (0 until 5).flatMap { brokenGroups })
+    fun unfold(n: Int = 5): Row {
+        return Row((0 until n).map { spec }.joinToString("?") { it }, (0 until n).flatMap { brokenGroups })
     }
 
-    fun getNthBit(i: Int, n: Int): Int {
-        val mask = 1 shl n
-        return (i and mask) shr n
+    fun specMatchesPositions(positions: List<Int>): Boolean {
+        val instance = (0 until spec.length).map {
+            '.'
+        }.toMutableList()
+        positions.forEachIndexed { idx, pos ->
+            (pos until pos + brokenGroups[idx]).forEach {
+                instance[it] = '#'
+            }
+        }
+
+        return specMatchesInstance(instance)
+    }
+
+    private fun specMatchesInstance(instance: MutableList<Char>) = instance.zip(spec.toList()).all {
+        (it.second == '?' || it.first == it.second)
     }
 
     fun matchesInstance(instance: String): Boolean {
@@ -57,12 +135,14 @@ data class Day12(val rows: List<Row>) {
         }
     }
 
-    fun part1(): Int {
+    fun part1(): Long {
         return rows.sumOf { it.countArrangements() }
     }
 
-    fun part2(): Int {
-        return rows.map { it.unfold() }.sumOf { it.countArrangements() }
+    fun part2(): Long {
+        return rows.map { it.unfold() }.sumOf {
+            it.countArrangements()
+        }
     }
 
 
@@ -81,5 +161,9 @@ fun createBitSet(n: Int): BitSet {
 fun main() {
     val day = Day12.parseInput("input")
     println("Part 1: ${day.part1()}")
+    //499500 is too low
+    //55672047646 is too low
+    //433520951429150 is too high
+    //17391848518844
     println("Part 2: ${day.part2()}")
 }
--- kotlin/src/test/kotlin/info/vervaeke/aoc2023/day12/Day12Test.kt
+++ kotlin/src/test/kotlin/info/vervaeke/aoc2023/day12/Day12Test.kt
@@ -13,27 +13,81 @@ class Day12Test {
         assertThat(day12.part1())
             .isEqualTo(21)
     }
-    
+
     @Test
-    fun parse() {
-        assertThat(day12.rows[0])
-            .isEqualTo(Row("???.###", listOf(1,1,3)))
+    fun part1Real() {
+        assertThat(Day12.parseInput("input").part1())
+            .isEqualTo(7047)
     }
 
     @Test
-    fun countArrangements() {
+    fun parse() {
+        assertThat(day12.rows[0])
+            .isEqualTo(Row("???.###", listOf(1, 1, 3)))
+    }
+
+    @Test
+    fun atTest() {
+        assertThat(day12.rows[0].groupCanBeAt(2, 4))
+            .isTrue()
+    }
+
+    @Test
+    fun row0_group1_cannotBeAt6() {
+        assertThat(day12.rows[0].groupCanBeAt(1, 6))
+            .isFalse()
+
+    }
+
+    @Test
+    fun countArrangements_row0() {
         assertThat(day12.rows[0].countArrangements())
             .isEqualTo(1)
+    }
+
+    @Test
+    fun countArrangements_row1() {
         assertThat(day12.rows[1].countArrangements())
             .isEqualTo(4)
+    }
+
+    @Test
+    fun canBeAt_row1() {
+//        assertThat(day12.rows[1].groupCanBeAt(0, 0))
+//            .isFalse()
+        assertThat(day12.rows[1].groupCanBeAt(0, 1))
+            .isTrue()
+
+    }
+
+    @Test
+    fun countArrangements_row2() {
         assertThat(day12.rows[2].countArrangements())
             .isEqualTo(1)
+    }
+
+    @Test
+    fun countArrangements_row3() {
         assertThat(day12.rows[3].countArrangements())
             .isEqualTo(1)
+    }
+
+    @Test
+    fun countArrangements_row4() {
         assertThat(day12.rows[4].countArrangements())
             .isEqualTo(4)
+    }
+
+    @Test
+    fun countArrangements_row5() {
         assertThat(day12.rows[5].countArrangements())
             .isEqualTo(10)
+    }
+
+    @Test
+    fun `row5_group2_cannotBeAtPosition2() {`() {
+        assertThat(day12.rows[5].groupCanBeAt(2, 2))
+            .isFalse()
     }
 
     @Test
@@ -45,14 +99,70 @@ class Day12Test {
     @Test
     fun createBitSet() {
         assertThat(createBitSet(5))
-            .isEqualTo(BitSet().apply { set(0); set (2) })
+            .isEqualTo(BitSet().apply { set(0); set(2) })
         assertThat(createBitSet(4))
-            .isEqualTo(BitSet().apply { set (2) })
+            .isEqualTo(BitSet().apply { set(2) })
+    }
+
+    @Test
+    fun unfold() {
+        assertThat(day12.rows[0].unfold())
+            .isEqualTo(Row("???.###????.###????.###????.###????.###", listOf(1, 1, 3, 1, 1, 3, 1, 1, 3, 1, 1, 3, 1, 1, 3)))
+    }
+
+    @Test
+    fun part2_countArrangements() {
+        assertThat(day12.rows[0].unfold().countArrangements())
+            .isEqualTo(1L)
+        assertThat(day12.rows[1].unfold().countArrangements())
+            .isEqualTo(16384L)
+        assertThat(day12.rows[2].unfold().countArrangements())
+            .isEqualTo(1L)
+        assertThat(day12.rows[3].unfold().countArrangements())
+            .isEqualTo(16L)
+        assertThat(day12.rows[4].unfold().countArrangements())
+            .isEqualTo(2500L)
+        assertThat(day12.rows[5].unfold().countArrangements())
+            .isEqualTo(506250L)
+    }
+
+    @Test
+    fun part2_unfold2_canBeAt() {
+        val row = day12.rows[2].unfold(2)
+        assertThat((0 until row.spec.length).filter { row.groupCanBeAt(0, it) })
+            .isEqualTo(listOf(1))
+        assertThat((0 until row.spec.length).filter { row.groupCanBeAt(1, it) })
+            .isEqualTo(listOf(1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27))
+        assertThat((0 until row.spec.length).filter { row.groupCanBeAt(2, it) })
+            .isEqualTo(listOf(1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29))
+        assertThat((0 until row.spec.length).filter { row.groupCanBeAt(3, it) })
+            .isEqualTo(listOf(9, 16))
+        assertThat((0 until row.spec.length).filter { row.groupCanBeAt(4, it) })
+            .isEqualTo(listOf(1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29))
+        assertThat((0 until row.spec.length).filter { row.groupCanBeAt(5, it) })
+            .isEqualTo(listOf(1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27))
+        assertThat((0 until row.spec.length).filter { row.groupCanBeAt(6, it) })
+            .isEqualTo(listOf(1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29))
+        assertThat((0 until row.spec.length).filter { row.groupCanBeAt(7, it) })
+            .isEqualTo(listOf(25))
+    }
+
+    @Test
+    fun part2_unfold2() {
+        val row = day12.rows[2].unfold(2)
+        assertThat(row.countArrangements())
+            .isEqualTo(1L)
     }
 
     @Test
     fun part2() {
         assertThat(day12.part2())
-            .isEqualTo(42)
+            .isEqualTo(525152L)
+    }
+
+    @Test
+    fun part2_real() {
+        assertThat(Day12.parseInput("input").part2())
+            .isEqualTo(17391848518844L)
     }
 }
