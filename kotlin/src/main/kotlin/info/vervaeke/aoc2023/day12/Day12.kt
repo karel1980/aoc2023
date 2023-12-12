3b95f282bae3a9e7eaf7553122f1ec92dd7134d5
Karel Vervaeke <karel.vervaeke@conundra.eu>
2023-12-12 10:09:21 +0100

    day12 part1
    part2 naive implementation is done but obviously it's too slow

--- kotlin/src/main/kotlin/info/vervaeke/aoc2023/day12/Day12.kt
+++ kotlin/src/main/kotlin/info/vervaeke/aoc2023/day12/Day12.kt
@@ -1,23 +1,81 @@
 package info.vervaeke.aoc2023.day12
 
-data class Day12(val lines: List<String>) {
+import java.util.*
+import java.util.regex.Pattern
+
+data class Row(val spec: String, val brokenGroups: List<Int>) {
+    val unknowns = spec.indices.filter { spec[it] == '?' }.mapIndexed { idx, pos -> pos to idx }.toMap()
+
+    val pattern = Pattern.compile("\\.+")
+
+    val options = ".#"
+
+    fun countArrangements(): Int {
+        if (unknowns.size == 0) {
+            return 1
+        }
+
+        val numberOfCombinations = Math.pow(2.toDouble(), unknowns.size.toDouble()).toInt()
+        return (0 until numberOfCombinations).count { n ->
+            val bitset = createBitSet(n)
+            val instance = spec.indices.map {
+                spec[it].takeIf { it != '?' } ?: options[if (bitset[unknowns[it]!!]) {
+                    0
+                } else {
+                    1
+                }]
+            }.joinToString("") { it.toString() }
+            matchesInstance(instance)
+        }
+    }
+
+    fun unfold(): Row {
+        return Row(listOf(spec, spec, spec, spec).joinToString("?") { it }, (0 until 5).flatMap { brokenGroups })
+    }
+
+    fun getNthBit(i: Int, n: Int): Int {
+        val mask = 1 shl n
+        return (i and mask) shr n
+    }
+
+    fun matchesInstance(instance: String): Boolean {
+        val parts = instance.split(pattern).filter { !it.isEmpty() }
+        return parts.map { it.length }.equals(brokenGroups)
+    }
+}
+
+data class Day12(val rows: List<Row>) {
     companion object {
         fun parseInput(path: String) = parseLines(readInput(path))
         fun readInput(path: String) = javaClass.getResource(path)!!.readText().lines()
         fun parseLines(lines: List<String>): Day12 {
-            return Day12(lines)
+            return Day12(lines.map { parseRow(it) })
+        }
+
+        fun parseRow(line: String) = line.split(" ").let {
+            Row(it[0], it[1].split(",").map { it.toInt() })
         }
     }
 
-    fun part1(): Long {
-        return 42
+    fun part1(): Int {
+        return rows.sumOf { it.countArrangements() }
     }
 
-    fun part2(): Long {
-        return 42
+    fun part2(): Int {
+        return rows.map { it.unfold() }.sumOf { it.countArrangements() }
     }
 
 
+}
+
+fun createBitSet(n: Int): BitSet {
+    return BitSet().apply {
+        n.toBigInteger().toString(2).reversed().forEachIndexed { idx, c ->
+            if (c == '1') {
+                set(idx)
+            }
+        }
+    }
 }
 
 fun main() {
--- kotlin/src/test/kotlin/info/vervaeke/aoc2023/day12/Day12Test.kt
+++ kotlin/src/test/kotlin/info/vervaeke/aoc2023/day12/Day12Test.kt
@@ -1,8 +1,8 @@
 package info.vervaeke.aoc2023.day12
 
-import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.util.*
 
 class Day12Test {
 
@@ -11,7 +11,43 @@ class Day12Test {
     @Test
     fun part1() {
         assertThat(day12.part1())
-            .isEqualTo(42)
+            .isEqualTo(21)
+    }
+    
+    @Test
+    fun parse() {
+        assertThat(day12.rows[0])
+            .isEqualTo(Row("???.###", listOf(1,1,3)))
+    }
+
+    @Test
+    fun countArrangements() {
+        assertThat(day12.rows[0].countArrangements())
+            .isEqualTo(1)
+        assertThat(day12.rows[1].countArrangements())
+            .isEqualTo(4)
+        assertThat(day12.rows[2].countArrangements())
+            .isEqualTo(1)
+        assertThat(day12.rows[3].countArrangements())
+            .isEqualTo(1)
+        assertThat(day12.rows[4].countArrangements())
+            .isEqualTo(4)
+        assertThat(day12.rows[5].countArrangements())
+            .isEqualTo(10)
+    }
+
+    @Test
+    fun matchesInstance() {
+        assertThat(day12.rows[0].matchesInstance("#.#.###"))
+            .isTrue()
+    }
+
+    @Test
+    fun createBitSet() {
+        assertThat(createBitSet(5))
+            .isEqualTo(BitSet().apply { set(0); set (2) })
+        assertThat(createBitSet(4))
+            .isEqualTo(BitSet().apply { set (2) })
     }
 
     @Test
