7c7a2a4564159b136960b2ed1678ac0b54796afd
Karel Vervaeke <karel.vervaeke@conundra.eu>
2023-12-14 12:49:38 +0100

    Day 14 part 1 & 2. So many off by ones it isn't funny anymore

--- kotlin/src/main/kotlin/info/vervaeke/aoc2023/day14/Day14.kt
+++ kotlin/src/main/kotlin/info/vervaeke/aoc2023/day14/Day14.kt
@@ -1,21 +1,115 @@
-package info.vervaeke.aoc2023.day13
+package info.vervaeke.aoc2023.day14
 
+data class Day14(val lines: List<MutableList<String>>) {
+    val rows = lines.size
+    val cols = lines[0].size
 
-data class Day14(val lines: List<String>) {
     companion object {
         fun parseInput(path: String) = parseLines(readInput(path))
         fun readInput(path: String) = javaClass.getResource(path)!!.readText().lines()
         fun parseLines(lines: List<String>): Day14 {
-            return Day14(lines)
+            return Day14(lines.map { it.toCharArray().map { it.toString() }.toMutableList() })
         }
     }
 
+    fun slideNorth(): Day14 {
+        val newLines = lines.map { it.toMutableList() }
+        (0 until cols).forEach {
+            slideNorth(newLines, it)
+        }
+        return Day14(newLines)
+    }
+
+    private fun slideNorth(newLines: List<MutableList<String>>, col: Int) {
+        val column = newLines.map { it[col] }
+        val roundRockPositions = column.indices.filter { column[it] == "O" }
+        roundRockPositions.forEach { pos ->
+            var newPos = pos
+            while (newPos > 0 && newLines[newPos - 1][col] == ".") {
+                newPos--
+            }
+            if (pos != newPos) {
+                newLines[newPos][col] = "O"
+                newLines[pos][col] = "."
+            }
+        }
+    }
+
+    fun render(): String {
+        return lines.joinToString("\n") { it.joinToString("") }
+    }
+
     fun part1(): Int {
-        return 42
+        return slideNorth().calculateWeight()
+    }
+
+    fun calculateWeight(): Int {
+        return (0 until cols).sumOf {
+            columnWeight(it)
+        }
+    }
+
+    fun columnWeight(col: Int): Int {
+        return (0 until rows).filter { lines[it][col] == "O" }
+            .map { rows - it }
+            .sum()
     }
 
     fun part2(): Int {
-        return 42
+        val n = 1_000_000_000 // the cycle that we're interested in
+        var result = this
+        var cycles = 0
+        val known = mutableMapOf<String, Int>()
+        while (result.signature() !in known) {
+            known[result.signature()] = cycles
+
+            result = result.cycle()
+            cycles++
+        }
+        val first = known[result.signature()]!!
+        val second = cycles
+        val cycleLength = second - first
+
+        val numCycles = (n - first) / cycleLength
+
+        val equivalentCycle = first + numCycles * cycleLength
+
+        println("first: $first")
+        println("second: $second")
+        println("cycleLength: $cycleLength")
+        println("result")
+        println(result.render())
+        println()
+
+        println("cycles: $cycles")
+        println("equivalent cycle: $equivalentCycle")
+
+        (equivalentCycle until n).forEach {
+            result = result.cycle()
+        }
+
+        return result.calculateWeight()
+    }
+
+    fun cycle(): Day14 {
+        var result = this
+        (0 until 4).forEach {
+            result = result.slideNorth()
+            result = result.rotateLeft()
+        }
+        return result
+    }
+
+    private fun signature() = render()
+
+    private fun rotateLeft(): Day14 {
+        val newLines = (0 until cols).map { col ->
+            (0 until rows).map { row ->
+                lines[rows - row - 1][col]
+            }.toMutableList()
+        }
+
+        return Day14(newLines)
     }
 }
 
--- kotlin/src/test/kotlin/info/vervaeke/aoc2023/day14/Day14Test.kt
+++ kotlin/src/test/kotlin/info/vervaeke/aoc2023/day14/Day14Test.kt
@@ -1,21 +1,116 @@
 package info.vervaeke.aoc2023.day14
 
-import info.vervaeke.aoc2023.day13.Day14
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class Day14Test {
-    val sample = Day14.parseInput("input")
+    val sample = Day14.parseInput("sample")
+
+    @Test
+    fun slideNorth_render() {
+        assertThat(sample.slideNorth().render())
+            .isEqualTo("""
+OOOO.#.O..
+OO..#....#
+OO..O##..O
+O..#.OO...
+........#.
+..#....#.#
+..O..#.O.O
+..O.......
+#....###..
+#....#....""".trimIndent())
+    }
+
+    @Test
+    fun cycle() {
+        assertThat(sample.cycle().render())
+            .isEqualTo("""
+.....#....
+....#...O#
+...OO##...
+.OO#......
+.....OOO#.
+.O#...O#.#
+....O#....
+......OOOO
+#...O###..
+#..OO#....
+""".trimIndent())
+    }
+
+    @Test
+    fun cycle2() {
+        assertThat(sample.cycle().cycle().render())
+            .isEqualTo("""
+.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#..OO###..
+#.OOO#...O
+""".trimIndent())
+    }
+
+    @Test
+    fun cycle3() {
+        assertThat(sample.cycle().cycle().cycle().render())
+            .isEqualTo("""
+.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#...O###.O
+#.OOO#...O
+""".trimIndent())
+    }
+
+    @Test
+    fun columnWeight() {
+        val north = sample.slideNorth()
+        assertThat(north.columnWeight(0))
+            .isEqualTo(10 + 9 + 8 + 7)
+        assertThat(north.columnWeight(1))
+            .isEqualTo(10 + 9 + 8)
+        assertThat(north.columnWeight(2))
+            .isEqualTo(10 + 4 + 3)
+        assertThat(north.columnWeight(3))
+            .isEqualTo(10)
+        assertThat(north.columnWeight(4))
+            .isEqualTo(8)
+        assertThat(north.columnWeight(5))
+            .isEqualTo(7)
+        assertThat(north.columnWeight(6))
+            .isEqualTo(7)
+        assertThat(north.columnWeight(7))
+            .isEqualTo(10 + 4)
+        assertThat(north.columnWeight(8))
+            .isEqualTo(0)
+        assertThat(north.columnWeight(9))
+            .isEqualTo(8 + 4)
+
+
+        assertThat(10 + 9 + 8 + 7 + 10 + 9 + 8 + 10 + 4 + 3 + 10 + 8 + 7 + 7 + 10 + 4 + 0 + 8 + 4)
+            .isEqualTo(136)
+    }
 
     @Test
     fun part1() {
         assertThat(sample.part1())
-            .isEqualTo(42)
+            .isEqualTo(136)
     }
 
     @Test
     fun part2() {
         assertThat(sample.part2())
-            .isEqualTo(42)
+            .isEqualTo(64)
     }
 }
