762d5b10245b6d62c21023998f71a05ad8e29def
Karel Vervaeke <karel.vervaeke@conundra.eu>
2023-12-20 10:08:08 +0100

    day19 part 2. blood sweat tears

--- kotlin/src/main/kotlin/info/vervaeke/aoc2023/day18/Day18.kt
+++ kotlin/src/main/kotlin/info/vervaeke/aoc2023/day18/Day18.kt
@@ -1,8 +1,6 @@
 package info.vervaeke.aoc2023.day18
 
 import info.vervaeke.aoc2023.day18.Direction.*
-import kotlin.math.max
-import kotlin.math.min
 
 enum class Direction(val drow: Int, val dcol: Int, val letter: String) {
     UP(-1, 0, "U"),
@@ -33,7 +31,11 @@ data class Coord(val row: Int, val col: Int) {
 }
 
 data class Day18(val digs: List<Dig>) {
-    val edgePoints = allEdgePoints()
+    val corners = determineCorners()
+    val edges = corners.indices.map { corners[it] to corners[(it + 1) % corners.size] }
+    val outlineArea = digs.sumOf { 0L + it.amount }
+    val verticalEdges = edges.filter { it.first.row != it.second.row }
+    val horizontalEdges = edges.filter { it.first.col != it.second.col }
 
     companion object {
         fun parseInput(path: String) = parseLines(readInput(path))
@@ -43,182 +45,109 @@ data class Day18(val digs: List<Dig>) {
         }.map {
             Dig(Direction.byLetter(it[0]), it[1].toInt(), it[2].substring(2, it[2].length - 1))
         })
-    }
-
-    fun part1(): Int {
-        return calculateSurface1()
-    }
-
-    fun calculateSurface1(): Int {
-        val points = getLeftEdgePoints()
-        return points.map {
-            // these are all points that are potentially inside
-            // but we must exclude points that are already inside
-            val point = it.first
-            val startCol = point.col
-
-            // this is the wall that we hit
-            val endCol = min(it.second.first.col, edgePoints.filter { it.row == point.row && it.col > point.col }.minOf { it.col })
-
-            point to endCol - startCol - 1
-        }.sortedBy { (point, count) ->
-            point.row
-        }.map { (point, count) ->
-            //            println("$point -- $count")
-            count
-        }
-            .sum() + allEdgePoints().size
-    }
-
-    fun getLeftEdgePoints(): List<Pair<Coord, Pair<Coord, Coord>>> {
-        // using raycasting -> find each point that is left of an inside part and the first part the ray would hit
-        val verticalPairs = getEdges(edgePoints)
-            .filter { it.first.col == it.second.col }
-
-        return edgePoints.map { point ->
-            // for every point, the list of walls that our ray (pointed to the right) would hit
-            point to verticalPairs.filter {
-                val top = min(it.first.row, it.second.row)
-                val bottom = max(it.first.row, it.second.row)
-                it.first.col > point.col && top < point.row && bottom >= point.row
-            }
-        }.filter {
-            // odd number of walls -> interior
-            it.second.size % 2 == 1
-        }.map {
-            // find the leftmost of these walls. That one determines how many (interior) cells to dig
-            val point = it.first
-            val wall = it.second.minBy { it.first.col }
-
-            point to wall
-        }
-    }
-
-    fun part2(): Int {
-        return swap().calculateSurface2()
-    }
-
-    fun calculateSurface2(): Int {
-        val cornerPoints = allCornerPoints()
-        val edges = getEdges(cornerPoints)
-        val verticalEdges = getVerticalEdges(edges)
-        val consideredPoints = calculateConsideredPoints(cornerPoints)
-        val pointsAndEdges = rayCastEdgeDetection(consideredPoints, edges, verticalEdges)
-        val insidePoints = keepOnlyInsidePoints(pointsAndEdges)
-        val pointsInside = pointsAndRowWidths(insidePoints)
-
-        val rows = consideredPoints.map { it.row }.distinct().sorted()
-        val rowToNext = rows.dropLast(1).zip(rows.drop(1)).toMap()
-        return pointsInside.sumOf { (point, cols) ->
-            val height = point.row - rowToNext[point.row]!! + 1
-            val width = cols
-            height * width
-        }
 
     }
 
-    fun pointsAndRowWidths(insidePoints: List<Pair<Coord, List<Pair<Coord, Coord>>>>) =
-        insidePoints.map { (coord, edges) ->
-            // for every coordinate, calculate how many squares are inside
-            coord to edges.map { it.first.col }.min() - coord.col - 1
-        }
+    fun part1(): Long {
+        val rows = corners.map { it.row }
+            .flatMap { listOf(it - 1, it, it + 1) }
+            .toSet()
+            .sorted()
 
-    fun keepOnlyInsidePoints(pointsAndEdges: List<Pair<Coord, List<Pair<Coord, Coord>>>>) =
-        pointsAndEdges.filter { (coord, edges) ->
-            // only keep coordinates that are inside
-            edges.size % 2 == 1
-        }
+        val cols = corners.map { it.col }
+            .flatMap { listOf(it - 1, it, it + 1) }
+            .toSet()
+            .sorted()
 
-    fun rayCastEdgeDetection(
-        consideredPoints: List<Coord>,
-        edges: List<Pair<Coord, Coord>>,
-        verticalEdges: List<Pair<Coord, Coord>>,
-    ) = getPointsNotInEdge(consideredPoints, edges).map {
-        val edgesHitByRayCast = verticalEdges.filter {
-            // only edges right of the current point considered
-                edge ->
-            it.col < edge.first.col
-        }.filter {
-            // count edges that our ray would hit. Note: ray goes to the right and very slightly up
-                edge ->
-            it.row > edge.first.row && it.row <= edge.second.row
-        }
-        it to edgesHitByRayCast
-    }
+        val rowSegments = rows.dropLast(1).zip(rows.drop(1)).map { it.first until it.second }
+        val colSegments = cols.dropLast(1).zip(cols.drop(1)).map { it.first until it.second }
 
-    fun getPointsNotInEdge(
-        consideredPoints: List<Coord>,
-        edges: List<Pair<Coord, Coord>>,
-    ) = consideredPoints.filter {
-        // only keep points that are not part of the edge
-        edges.none { edge -> edge.contains(it) }
-    }
-
-    fun calculateConsideredPoints(cornerPoints: List<Coord>) = cornerPoints.flatMap {
-        // points to consider
-        listOf(it + RIGHT)
-    }.distinct().sortedBy { it.row }
-
-    fun getVerticalEdges(edges: List<Pair<Coord, Coord>>) =
-        edges.filter { it.first.col == it.second.col }
-            .map {
-                // vertical edges are oriented from low to high rows
-                if (it.first.row < it.second.row) {
-                    it
+        var insideSurfaceArea = 0L
+        rowSegments.forEach { rowSegment ->
+            colSegments.forEach { colSegment ->
+                val a = Coord(rowSegment.first, colSegment.first)
+                val b = Coord(rowSegment.last, colSegment.last)
+                val height = 0L + rowSegment.last - rowSegment.first + 1
+                val width = 0L + colSegment.last - colSegment.first + 1
+                val surf = height * width
+                if (isInside(a)) {
+//                    println("IN: $a to $b:   adding $height x $width = $surf")
+                    insideSurfaceArea += surf
                 } else {
-                    it.second to it.first
-                }
-            }
-
-    fun getEdges(cornerPoints: List<Coord>) = cornerPoints.indices.map { cornerPoints[it] to cornerPoints[(it + 1) % cornerPoints.size] }
-
-    fun swap(): Day18 {
-        return Day18(digs.map { it.swap() })
-    }
-
-    fun allEdgePoints(): List<Coord> {
-        var current = Coord(0, 0)
-        return buildList {
-            digs.forEach { dig ->
-                (0 until dig.amount).forEach {
-                    current += dig.dir
-                    add(current)
+//                    println("OUT: $a to $b: not adding $height x $width = $surf")
                 }
             }
         }
+
+//        println(outlineArea)
+        return insideSurfaceArea + outlineArea;
     }
 
-    fun allCornerPoints(): List<Coord> {
-        var current = Coord(0, 0)
+    fun isInside(coord: Coord): Boolean {
+        if (verticalEdges.any { it.contains(coord) }) {
+            return false;
+        }
+        if (horizontalEdges.any { it.contains(coord) }) {
+            return false;
+        }
+
+        //ray casting
+        val hits = verticalEdges.filter { it.first.col > coord.col }
+            .filter {
+                (it.first.row < coord.row && it.second.row >= coord.row) ||
+                        it.second.row < coord.row && it.first.row >= coord.row
+            }
+            .count()
+
+        return hits % 2 == 1
+    }
+
+    fun determineCorners(): List<Coord> {
         return buildList {
-            digs.forEach { dig ->
-                current = Coord(current.row + dig.dir.drow * dig.amount, current.col + dig.dir.dcol * dig.amount)
+            var current = Coord(0, 0)
+            digs.forEach {
                 add(current)
+                current = Coord(current.row + it.dir.drow * it.amount, current.col + it.dir.dcol * it.amount)
             }
         }
     }
 
-    fun edgePoints(): Int {
-        return digs.sumOf { it.amount }
-    }
+    fun part2() = swap().part1()
 
+    fun swap() = Day18(digs.map { it.swap() })
 
 }
 
 private fun Pair<Coord, Coord>.contains(coord: Coord): Boolean {
-    if (this.first.row == this.second.row) {
-        return coord.row == this.first.row && coord.col in this.first.col..this.second.col
+    if (first.row == second.row) {
+        // horizontal edge
+        if (coord.row != first.row) {
+            return false
+        }
+        if (first.col < second.col) {
+            return coord.col in first.col..second.col
+        } else {
+            return coord.col in second.col .. first.col
+        }
+    } else {
+        // vertical
+        if (coord.col != first.col) {
+            return false
+        }
+        if (first.row < second.row) {
+            return coord.row in first.row .. second.row
+        } else {
+            return coord.row in second.row .. first.row
+        }
+
     }
-    if (this.first.col == this.second.col) {
-        return coord.col == this.first.col && coord.row in this.first.row..this.second.row
-    }
-    return false
 }
 
 fun main() {
     val day = Day18.parseInput("input")
     //6160 is too low
     println("Part 1: ${day.part1()}")
-    //1327053636 is too low
+
+    //83812607470235 is too high
     println("Part 2: ${day.part2()}")
 }
--- kotlin/src/test/kotlin/info/vervaeke/aoc2023/day18/Day18Test.kt
+++ kotlin/src/test/kotlin/info/vervaeke/aoc2023/day18/Day18Test.kt
@@ -21,137 +21,27 @@ class Day18Test {
     }
 
     @Test
-    fun points() {
-        assertThat(sample.edgePoints())
-            .isEqualTo(38)
-    }
-
-    @Test
-    fun getLeftEdgePoints() {
-        assertThat(sample.getLeftEdgePoints().filter { it.first.row < 3 })
-            .containsExactlyInAnyOrder(
-                Coord(1, 0) to (Coord(0, 6) to Coord(1, 6)),
-                Coord(2, 0) to (Coord(1, 6) to Coord(2, 6)),
-                Coord(2, 1) to (Coord(1, 6) to Coord(2, 6)),
-                Coord(2, 2) to (Coord(1, 6) to Coord(2, 6)),
-            )
-    }
-
-    @Test
-    fun part1_newWay() {
-        val cornerPoints = sample.allCornerPoints()
-        assertThat(cornerPoints)
-            .isEqualTo(
-                listOf(
-                    Coord(0, 6),
-                    Coord(5, 6),
-                    Coord(5, 4),
-                    Coord(7, 4),
-                    Coord(7, 6),
-                    Coord(9, 6),
-                    Coord(9, 1),
-                    Coord(7, 1),
-                    Coord(7, 0),
-                    Coord(5, 0),
-                    Coord(5, 2),
-                    Coord(2, 2),
-                    Coord(2, 0),
-                    Coord(0, 0),
-                )
-            )
-
-        val edges = sample.getEdges(cornerPoints)
-        assertThat(edges)
-            .isEqualTo(
-                listOf(
-                    Coord(0, 6) to Coord(5, 6),
-                    Coord(5, 6) to Coord(5, 4),
-                    Coord(5, 4) to Coord(7, 4),
-                    Coord(7, 4) to Coord(7, 6),
-                    Coord(7, 6) to Coord(9, 6),
-                    Coord(9, 6) to Coord(9, 1),
-                    Coord(9, 1) to Coord(7, 1),
-                    Coord(7, 1) to Coord(7, 0),
-                    Coord(7, 0) to Coord(5, 0),
-                    Coord(5, 0) to Coord(5, 2),
-                    Coord(5, 2) to Coord(2, 2),
-                    Coord(2, 2) to Coord(2, 0),
-                    Coord(2, 0) to Coord(0, 0),
-                    Coord(0, 0) to Coord(0, 6),
-
-                    )
-            )
-        val verticalEdges = sample.getVerticalEdges(edges)
-        assertThat(verticalEdges)
-            .isEqualTo(
-                listOf(
-                    Coord(0, 6) to Coord(5, 6),
-                    Coord(5, 4) to Coord(7, 4),
-                    Coord(7, 6) to Coord(9, 6),
-                    Coord(7, 1) to Coord(9, 1),
-                    Coord(5, 0) to Coord(7, 0),
-                    Coord(2, 2) to Coord(5, 2),
-                    Coord(0, 0) to Coord(2, 0),
-                )
-            )
-        val consideredPoints = sample.calculateConsideredPoints(cornerPoints)
-
-        val pointsAndEdges = sample.rayCastEdgeDetection(consideredPoints, edges, verticalEdges)
-        assertThat(pointsAndEdges)
-            .containsExactlyInAnyOrder(
-                Coord(-1, 6 + 1) to listOf(),
-                Coord(0, 6 + 1)  to listOf(),
-                Coord(1, 6 + 1)  to listOf(),
-                Coord(5 - 1, 6 + 1)  to listOf(),
-                Coord(5, 6 + 1)  to listOf(),
-                Coord(5 + 1, 6 + 1)  to listOf(),
-                Coord(5 - 1, 4 + 1)  to listOf(),
-                Coord(5, 4 + 1)  to listOf(),
-                Coord(5 + 1, 4 + 1)  to listOf(),
-                Coord(7 - 1, 4 + 1)  to listOf(),
-                Coord(7, 4 + 1)  to listOf(),
-                Coord(7 + 1, 4 + 1)  to listOf(),
-                Coord(7 - 1, 6 + 1)  to listOf(),
-                Coord(7, 6 + 1)  to listOf(),
-                Coord(7 + 1, 6 + 1)  to listOf(),
-                Coord(9 - 1, 6 + 1)  to listOf(),
-                Coord(9, 6 + 1)  to listOf(),
-                Coord(9 + 1, 6 + 1)  to listOf(),
-                Coord(9 - 1, 1 + 1)  to listOf(),
-                Coord(9, 1 + 1)  to listOf(),
-                Coord(9 + 1, 1 + 1)  to listOf(),
-                Coord(7 - 1, 1 + 1)  to listOf(),
-                Coord(7, 1 + 1)  to listOf(),
-                Coord(7 + 1, 1 + 1)  to listOf(),
-                Coord(7 - 1, 1)  to listOf(),
-                Coord(7, 1)  to listOf(),
-                Coord(7 + 1, 1)  to listOf(),
-                Coord(5 - 1, 1)  to listOf(),
-                Coord(5, 1)  to listOf(),
-                Coord(5 + 1, 1)  to listOf(),
-                Coord(5 - 1, 2 + 1)  to listOf(),
-                Coord(5, 2 + 1)  to listOf(),
-                Coord(5 + 1, 2 + 1)  to listOf(),
-                Coord(2 - 1, 2 + 1)  to listOf(),
-                Coord(2, 2 + 1)  to listOf(),
-                Coord(2 + 1, 2 + 1)  to listOf(),
-                Coord(2 - 1, 1)  to listOf(),
-                Coord(2, 1)  to listOf(),
-                Coord(2 + 1, 1)  to listOf(),
-                Coord(-1, 1)  to listOf(),
-                Coord(0, 1)  to listOf(),
-                Coord(1, 1) to listOf())
-        val insidePoints = sample.keepOnlyInsidePoints(pointsAndEdges)
-        val pointsInside = sample.pointsAndRowWidths(insidePoints)
-
-//        assertThat(sample.calculateSurface2())
-//            .isEqualTo(62)
-    }
-
-    @Test
     fun part1() {
         assertThat(sample.part1())
             .isEqualTo(62)
+    }
+
+    @Test
+    fun part1_square() {
+        assertThat(Day18.parseInput("square").part1())
+            .isEqualTo(30)
+    }
+
+    @Test
+    fun isInside() {
+        assertThat(sample.isInside(Coord(1,-1)))
+            .isFalse()
+    }
+
+    @Test
+    fun isInside_onEdge() {
+        assertThat(sample.isInside(Coord(1,0)))
+            .isFalse()
     }
 
     @Test
@@ -160,9 +50,10 @@ class Day18Test {
             .isEqualTo(70026)
     }
 
-//    @Test
-//    fun part2() {
-//        assertThat(sample.part2())
-//            .isEqualTo(952408144115)
-//    }
+    @Test
+    fun part2() {
+        assertThat(sample.part2())
+            .isEqualTo(952408144115)
+    }
+
 }
